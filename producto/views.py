09409873d537da09bb3d11e828cbc12132a5cd2e
Ivaname99 <mg0913032023@unab.edu.sv>
2023-10-28 22:58:41 -0600

    Primer despliegue

---
+++ producto/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import redirect, render
+from .models import Producto
+from .forms import ProductoForm
+
+# Create your views here.
+
+def inicio(request):
+    return render(request, 'pages/inicio.html')
+
+def listado_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'producto/productos.html', { 'productos': productos })
+
+def crear_producto(request):
+    formulario = ProductoForm(request.POST or None)
+
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('productos')        
+
+    return render(request, 'producto/crear.html', {'formulario': formulario})
+
+def editar_producto(request, id):
+    producto = Producto.objects.get(id = id)
+
+    formulario = ProductoForm(request.POST or None, instance = producto)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('productos') 
+    
+    return render(request, 'producto/editar.html', {'formulario': formulario})
+
+def eliminar_producto(request, id):
+    producto = Producto.objects.get(id = id)
+    producto.delete()
+    return redirect('productos')
